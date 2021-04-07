339cfdb97e43f5ed6b5fac67098b49ab6d841893
Vitalii Syrykh <vitaliysyryck@gmail.com>
2021-04-07 14:19:30 +0300

    add node modules

---
+++ server.js
@@ -0,0 +1,11 @@
+const http = require('http');
+require('dotenv').config();
+const app = require('./app.js');
+
+const server = http.createServer(app);
+
+const port = process.env.PORT || 3001;
+
+server.listen(port, () => {
+  console.log(`APP started on port ${port}`);
+});
