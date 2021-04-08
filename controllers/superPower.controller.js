66df9a0a734454c3c776325967122868a9b22fa5
Vitalii Syrykh <vitaliysyryck@gmail.com>
2021-04-08 20:57:22 +0300

    Add getSuperPower deleteSuperPower

--- controllers/superPower.controller.js
+++ controllers/superPower.controller.js
@@ -1,11 +1,36 @@
-const {superPowers} = require('../models');
+const { SuperPowers } = require('../models');
 
-module.exports.createSuperPower = async (req, res, next) =>{
+module.exports.createSuperPower = async (req, res, next) => {
   try {
-    const {heroInstance, body}=req;
+    const { heroInstance, body } = req;
     const addedPowers = await heroInstance.createSuperPower(body);
     res.send(addedPowers);
   } catch (err) {
-    next(err)
+    next(err);
   }
-}
+};
+
+module.exports.getSuperPower = async (req, res, next) => {
+  try {
+    const { heroInstance } = req;
+    const SuperPowers = await heroInstance.getSuperPowers();
+    res.status(200).send(SuperPowers);
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports.getPower = async (req, res, next) => {};
+
+module.exports.deleteSuperPower = async (req, res, next) => {
+  try {
+    const {
+      params: { idPower },
+    } = req;
+    const supPower = await SuperPowers.findByPk(+idPower);
+    await supPower.destroy({ returning: true });
+    res.send(supPower);
+  } catch (err) {
+    next(err);
+  }
+};
