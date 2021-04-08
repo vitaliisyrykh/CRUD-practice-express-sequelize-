b781961ab517b07ceba9c58ef118bc22941df6c9
Vitalii Syrykh <vitaliysyryck@gmail.com>
2021-04-08 10:22:28 +0300

    add checkHeroMw

---
+++ middlewares/checkHero.mw.js
@@ -0,0 +1,20 @@
+const { SuperHeroes } = require('../models/SuperHeroes');
+
+module.exports.checkHero = async (req, res, next) => {
+  try {
+    const {
+      params: { idHero },
+    } = req;
+    
+    const heroInstance = await SuperHeroes.findByPk(idHero);
+    if (!heroInstance) {
+      throw new Error('Hero not found');
+      return next(err)
+    };
+    console.log(heroInstance);
+    req.heroInstance = heroInstance;
+    next();
+  } catch (err) {
+    next(err);
+  }
+};
